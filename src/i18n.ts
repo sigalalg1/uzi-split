1f8769300a02ce577b7c15f78c765d7a6a4fcbb1
salgranaty <salgranaty@nvidia.com>
2026-01-17 14:13:22 +0200

    added local support

---
+++ src/i18n.ts
@@ -0,0 +1,23 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import enTranslations from "./locales/en.json";
+import heTranslations from "./locales/he.json";
+
+const resources = {
+  en: enTranslations,
+  he: heTranslations,
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: localStorage.getItem("language") || "en",
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
+
